fc9c07a11ecb901e10a0a8ea37bc0d4bfd72f18b
Ahmet <110814262+zapkinus11@users.noreply.github.com>
2024-02-07 16:11:48 +0300

    grade_calculation
    Simple midterm and final grade calculation code written in C++. (midterm exam 40%, final 60%)

---
+++ grade_calculation/grade_calculation/grade_calculation.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+
+	int note1, note2, average;
+
+	cout << "Enter your visa note: ";
+	cin >> note1;
+
+	cout << "Enter your final note: ";
+	cin >> note2;
+
+	average = note1 * 0.4 + note2 * 0.6;
+
+	if (average >= 50)
+	{
+		cout << " " << "\n";
+		cout << "Your midterm and final GPA = " << average << " Congratulations, you got a passing grade in the course." << "\n";
+		cout << " ";
+	}
+	else {
+		cout << " " << "\n";
+		cout << "Your midterm and final GPA = " << average << " You did not get a passing grade in the course." << "\n";
+		cout << " ";
+	}
+}
